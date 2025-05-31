0e1c1e4f6cb3a695cb04da592ce8ab06c1adb145
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-06-01 01:22:21 +0900

    chore: eslintの設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,55 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'object-shorthand': 'error',
+      'react/jsx-curly-brace-presence': 'error',
+      'react/self-closing-comp': [
+        'error',
+        {
+          component: true,
+          html: false,
+        },
+      ],
+      '@next/next/no-img-element': 'off',
+    },
+  },
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      'build',
+      'coverage',
+      '.next',
+      'out',
+      'public/static',
+      'public/assets',
+      'public/favicon.ico',
+      'public/robots.txt',
+    ],
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
