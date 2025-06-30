26d970a2480794e6265eca91e9f33b7b85c40b61
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-06-30 14:54:00 +0000

    add: エンドポイント周りの型定義を行い、型安全にgetList等を扱えるように

---
+++ src/lib/microcms/types.ts
@@ -0,0 +1,69 @@
+import type {
+  GetListRequest as MicroCMSGetListRequest,
+  MicroCMSImage,
+  MicroCMSListContent,
+  MicroCMSListResponse,
+  MicroCMSQueries,
+} from 'microcms-js-sdk';
+
+// EndPoints
+type Movies = {
+  title: string;
+  description: string;
+  url: `https://www.youtube.com/embed/${string}?si=${string}`;
+  startAt?: number;
+  thumbnail?: MicroCMSImage;
+} & MicroCMSListContent;
+
+type Images = {
+  title: string;
+  description: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
+type Member = {
+  name: string;
+  description: string;
+  role: string;
+} & MicroCMSListContent;
+
+type Biographies = {
+  title: string;
+  description: string;
+  dateAt: string;
+} & MicroCMSListContent;
+
+type Endpoints = {
+  movies: Movies;
+  images: Images;
+  members: Member;
+  biographies: Biographies;
+};
+
+// MicroCMS Types
+type GetListQueries<F> = {
+  fields?: F[];
+} & Omit<MicroCMSQueries, 'fields'>;
+
+type GetListRequest<E extends keyof Endpoints, F extends keyof Endpoints[E]> = {
+  endpoint: E;
+  queries?: GetListQueries<F>;
+} & Omit<MicroCMSGetListRequest, 'endpoint' | 'queries'>;
+
+type GetListResponse<T, F extends keyof T> = {
+  contents: Pick<T, F>[];
+  totalCount: number;
+  offset: number;
+  limit: number;
+} & Omit<MicroCMSListResponse<T>, 'contents'>;
+
+export type {
+  Biographies,
+  Endpoints,
+  GetListQueries,
+  GetListRequest,
+  GetListResponse,
+  Images,
+  Member,
+  Movies,
+};
