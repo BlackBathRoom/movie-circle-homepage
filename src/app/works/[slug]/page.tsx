f5b0faaaed00f6a7b1b225ac38d1c4c912a6e8f8
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-07-22 06:04:53 +0000

    add: worksに詳細ページを追加し動画再生を行うように
    works直下ではサムネイルのスライダー

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -1,12 +1,35 @@
+import MoviePlayer from '@/components/MoviePlayer';
 import { client } from '@/lib/microcms';
 import type { NextPage } from 'next';
-const Page: NextPage = async () => {
-  const movie = await client
-    .getList({
-      endpoint: 'movies',
-      queries: { fields: ['url', 'title', 'startAt'] },
-    })
-    .then((res) => res.contents[0]);
+
+type Props = {
+  params: { slug: string };
+};
+
+const Page: NextPage<Props> = async ({ params }) => {
+  const { slug } = await params;
+  const movie = await client.getListDetail({
+    endpoint: 'movies',
+    contentId: slug,
+    queries: { fields: ['url', 'title', 'startAt', 'description'] },
+  });
+
+  return (
+    <div className="px-3 py-2 flex flex-col items-center gap-5">
+      <div className="flex flex-col items-center">
+        <MoviePlayer
+          href={movie.url}
+          embedVideoTitle={movie.title}
+          startAt={movie.startAt}
+          className=""
+        />
+        <span className="text-2xl font-bold text-gray-800 mt-4">
+          {movie.title}
+        </span>
+      </div>
+      <span className="text-lg text-gray-700">{movie.description}</span>
+    </div>
+  );
 };
 
 export default Page;
--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,37 +1,40 @@
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-import MovieWorks from '@/components/MovieWorks';
+import Slider from '@/components/Slider';
 import { client } from '@/lib/microcms';
 import type { NextPage } from 'next';
 
 const Page: NextPage = async () => {
-  const movie = await client
+  const movieThumbnails = await client
     .getList({
       endpoint: 'movies',
-      queries: { fields: ['url', 'title', 'startAt'] },
+      queries: { fields: ['id', 'title', 'thumbnail'] },
     })
-    .then((res) => res.contents[0]);
+    .then((res) =>
+      res.contents.map((content) => ({
+        id: content.id,
+        src: content.thumbnail?.url || '',
+        alt: content.title,
+        width: content.thumbnail?.width,
+        height: content.thumbnail?.height,
+      })),
+    );
 
   return (
-    <div className="m-0 p-0">
-      <Header />
-      <main className="min-h-screen flex flex-col items-center justify-center bg-gray-600 py-12 px-4">
-        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold mb-9 text-white text-center">
-          ProductionMET
-        </h1>
-        <section className=" relative w-full h-screen overflow-hidden ">
-          <MovieWorks
-            movie={{
-              url: movie.url,
-              title: movie.title,
-              startAt: movie.startAt,
-            }}
-            className="bg-black rounded-lg shadow-xl  w-full h-full"
-          />
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-600 py-12 px-4">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold mb-9 text-white text-center">
+        ProductionMET
+      </h1>
+      <section className=" relative w-full h-screen overflow-hidden ">
+        <Slider
+          images={movieThumbnails.map((thumbnail) => ({
+            src: thumbnail.src,
+            alt: thumbnail.alt,
+            width: thumbnail.width,
+            height: thumbnail.height,
+            href: `/works/${thumbnail.id}`,
+          }))}
+        />
+      </section>
+    </main>
   );
 };
 
