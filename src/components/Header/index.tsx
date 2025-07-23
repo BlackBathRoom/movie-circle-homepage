d239ee3373eeaaa3ca3e00d1cfba1c459bd3765b
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-07-23 05:54:55 +0000

    add: ロゴをwebpに変換し、長いロゴも追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="bg-white shadow-md">
       <div className="flex w-full justify-between px-5 py-3">
         <Link href="/" className="shadow-2xs hover:shadow-lg hover:underline">
-          <img src="MET.png" alt="logo" className="h-10 w-auto" />
+          <img src="met-logo.webp" alt="logo" className="h-10 w-auto" />
         </Link>
         <nav className="w-full flex justify-end">
           <ul className="flex text-gray-700 font-medium gap-4">
