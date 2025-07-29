566b8be0bf833d35fdb70d53c6d17ae9a47c2994
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-07-29 14:11:59 +0000

    chore: makeコマンドを修正

--- scripts/template/StoryTemplate.stories.ts
+++ scripts/template/StoryTemplate.stories.ts
@@ -1,8 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+
 import __COMPONENT__ from './index';
 
 const meta: Meta<typeof __COMPONENT__> = {
-  title: 'Components/__COMPONENT__',
+  title: 'Components/__TYPE__/__COMPONENT__',
   component: __COMPONENT__,
   parameters: {
     layout: 'center',
